094c8f44068229f08575b9e9519fd4b4cf5debd8
bolekhan1508 <bolehanw@gmail.com>
2020-03-09 13:37:12 +0200

    my homework done

---
+++ lantern/git/homework.py
@@ -0,0 +1,164 @@
+"""
+This is a list of functions that should be completed.
+"""
+
+from typing import Any
+from typing import List
+
+
+class OurAwesomeException(Exception):
+    pass
+
+
+def is_two_object_has_same_value(first: Any, second: Any) -> bool:
+    """
+    If @first and @second has same value should return True
+    In another case should return False
+    """
+    return first == second
+
+
+def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
+    """
+    If @first and @second has same type should return True
+    In another case should return False
+    """
+    return type(first) == type(second)
+
+
+def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
+    """
+    If @first and @second has same type should return True
+    In another case should return False
+    """
+    return first is second
+
+
+def multiple_ints(first_value: int, second_value: int) -> int:
+    """
+    Should calculate product of all args.
+    if first_value or second_value is not int should raise TypeError
+
+    Raises:
+        TypeError
+
+    Params:
+        first_value: value for multiply
+        second_value
+    Returns:
+        Product of elements
+    """
+    x = first_value * second_value
+    if not type(x) is int:
+        raise TypeError
+    else:
+        return x
+
+
+def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
+    """
+    If possible to convert arguments to int value - convert and multiply them.
+    If it is impossible raise ValueError
+
+    Args:
+        first_value: number for multiply
+        second_value: number for multiply
+
+    Raises:
+        ValueError
+
+    Returns: multiple of two numbers.
+
+    Examples:
+        multiple_ints_with_conversion(6, 6)
+        >>> 36
+        multiple_ints_with_conversion(2, 2.0)
+        >>> 4
+        multiple_ints_with_conversion("12", 1)
+        >>> 12
+        try:
+            multiple_ints_with_conversion("Hello", 2)
+        except ValueError:
+            print("Not valid input data")
+        >>> "Not valid input data"
+    """
+    try:
+        x = int(first_value) * int(second_value)
+        return x
+    except TypeError:
+        print("Not valid input data")
+
+
+
+
+def is_word_in_text(word: str, text: str) -> bool:
+    """
+    If text contain word return True
+    In another case return False.
+
+    Args:
+        word: Searchable substring
+        text: Text for search
+
+    Examples:
+        is_word_in_text("Hello", "Hello word")
+        >>> True
+        is_word_in_text("Glad", "Nice to meet you ")
+        >>> False
+
+    """
+    if word in text:
+        return True
+    else:
+        return False
+
+
+def some_loop_exercise() -> list:
+    """
+    Use loop to create list that contain int values from 0 to 12 except 6 and 7
+    """
+    arr = []
+    for i in range(13):
+        if i != 6 and i != 7:
+            arr.append(i)
+    return arr
+
+
+def remove_from_list_all_negative_numbers(data: List[int]) -> list:
+    """
+    Use loops to solve this task.
+    You could use data.remove(negative_number) to solve this issue.
+    Also you could create new list with only positive numbers.
+    Examples:
+        remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
+        >>> [1, 5, 8]
+    """
+    arr = []
+    for i in data:
+        if i >= 0:
+            arr.append(i)
+    return arr
+
+
+def alphabet() -> dict:
+    """
+    Create dict which keys are alphabetic characters. And values their number in alphabet
+    Notes You could see an implementaion of this one in test, but create another one
+    Examples:
+        alphabet()
+        >>> {"a": 1, "b": 2 ...}
+    """
+    dict = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l',
+                    13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w',
+                    24: 'x', 25: 'y', 26: 'z'}
+    return dict
+
+
+def simple_sort(data: List[int]) -> List[list]:
+    """
+    Sort list of ints without using built-in methods.
+    Examples:
+        simple_sort([2, 9, 6, 7, 3, 2, 1])
+        >>> [1, 2, 2, 3, 6, 7, 9]
+    """
+    return sorted(data)
